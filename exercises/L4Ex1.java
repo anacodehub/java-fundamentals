16c82f22c14b4fc79174bc6ec8553f6fd96ea152
Ana Marcondes <168790968+anacodehub@users.noreply.github.com>
2025-02-07 20:19:51 -0300

    Update L4Ex1.java
    finish L4Ex1

--- exercises/L4Ex1.java
+++ exercises/L4Ex1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public L4Ex1 {
+public class L4Ex1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -10,12 +10,16 @@ public L4Ex1 {
 
         if(input.hasNextInt()) {
             num1 = input.nextInt();
+        } else {
+            System.out.println("Valor do primeiro número inválido.");
         }
 
         System.out.print("Segundo numero: ");
 
         if(input.hasNextInt()) {
             num2 = input.nextInt();
+        } else {
+            System.out.println("Valor do segundo número inválido.");
         }
 
         System.out.print("Terceiro numero: ");
@@ -24,7 +28,15 @@ public L4Ex1 {
             num3 = input.nextInt();
         }
 
-        if()
+        else {
+            System.out.println("Valor do terceiro número inválido.");
+        }
 
+        int sum = num1+num2+num3;
+
+        String isDivisibleByFive = sum % 5 == 0 ? "eh divisivel" : "nao eh divisivel";
+
+
+        System.out.println("A soma dos numero "+isDivisibleByFive+" por cinco");
     }
 }
