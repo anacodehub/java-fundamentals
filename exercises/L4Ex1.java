20e455d848bb5580eba80dce5e3ed0f96565f506
Ana <anaprojectshub@users.noreply.github.com>
2025-02-06 21:56:17 -0300

    add ex 1 processing

--- exercises/L4Ex1.java
+++ exercises/L4Ex1.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public L4Ex1 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        int num1=0, num2=0, num3=0;
+
+        System.out.print("Primeiro numero: ");
+
+        if(input.hasNextInt()) {
+            num1 = input.nextInt();
+        }
+
+        System.out.print("Segundo numero: ");
+
+        if(input.hasNextInt()) {
+            num2 = input.nextInt();
+        }
+
+        System.out.print("Terceiro numero: ");
+
+        if(input.hasNextInt()) {
+            num3 = input.nextInt();
+        }
+
+        if()
+
     }
 }
