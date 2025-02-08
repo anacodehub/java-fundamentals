7f6c1dba7e6fb2424f626ef2f42b196e8dcef6bd
Ana Marcondes <168790968+anacodehub@users.noreply.github.com>
2025-02-08 12:36:21 -0300

    Create Ex1.java

---
+++ src/aulas/Ex1.java
@@ -0,0 +1,11 @@
+package aulas;//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Ex1 {
+    public static void main(String[] var0) {
+        System.out.println("Hello world! ");
+        System.out.print("Hello \nworld!");
+    }
+}
